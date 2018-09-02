4f18a5387ccaa921fc5f45b666fdcafee5380f74
Ravindra Khapate <Ravindra.Khapate@ey.com>
2018-09-02 20:44:12 +0530

    Unit test project added.

---
+++ test/HT.Interview.ChatBot.Test/Services/UserQueryServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using HT.Framework;
+using HT.Interview.ChatBot.Common.DTO;
+using HT.Interview.ChatBot.Common.Entities;
+using HT.Interview.ChatBot.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HT.Interview.ChatBot.Test.Services
+{
+    public class UserQueryServiceTests
+    {
+        public UserQueryServiceTests()
+        {
+            TestFactory testFactory = new TestFactory();
+            (Mock<UserService> mockService, Mock<DbSet<User>> mockDbSet) result = testFactory.CreateMockUserClaimQueryService();
+            Sut = result.mockService;
+            MockDbSet = result.mockDbSet;
+        }
+
+        public Mock<UserService> Sut { get; }
+        public Mock<DbSet<User>> MockDbSet { get; }
+
+        [Fact]
+        [Trait("Category", "Service_Query")]
+        public async Task GetUsersAsync_ReturnsUsers_GivenUsersExist()
+        {
+            //Arrange
+            UserQuery uq = new UserQuery();
+
+            //Act
+            Response<System.Collections.Generic.IEnumerable<User>> response = await Sut.Object.GetUsersAsync(uq);
+
+            //Assert
+            Assert.True(response.IsSuccess);
+            Assert.NotEmpty(response.Value);
+        } 
+    }
+}
