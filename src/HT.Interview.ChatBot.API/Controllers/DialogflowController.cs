f88f764a9d55ad80f6cfa5b12d353d0bc915bc72
Ravindra Khapate <Ravindra.Khapate@ey.com>
2018-09-09 02:10:10 +0530

    code changes

--- src/HT.Interview.ChatBot.API/Controllers/DialogflowController.cs
+++ src/HT.Interview.ChatBot.API/Controllers/DialogflowController.cs
@@ -68,13 +68,15 @@ namespace HT.Interview.ChatBot.API.Controllers
                     IntentsClient client = IntentsClient.Create();
                     foreach (IntentResponse intentResponse in intentList.OrderBy(x => x.ParentIntentId))
                     {
-                        Intent intent = new Intent();
-
-
+                        Intent intent = new Intent(); 
                         intent.DefaultResponsePlatforms.Add(Platform.ActionsOnGoogle);
                         intent.DisplayName = intentResponse.DisplayName;
                         intent.Messages.Add(AddIntentDefault(intentResponse.Text));
-
+                         
+                        if (intentResponse.ParentIntentId != null)
+                        {
+                            intent.ParentFollowupIntentName = intentList.Where(x => x.Id == intentResponse.ParentIntentId).FirstOrDefault().IntentName;
+                        }
 
                         if (intentResponse.IntentTrainingPhraseResponse.Any())
                         {
@@ -98,11 +100,6 @@ namespace HT.Interview.ChatBot.API.Controllers
                             {
                                 intent.Messages.Add(AddIntentSuggestion(suggestion.Title));
                             }
-                        }
-
-                        if (intentResponse.ParentIntentId != null)
-                        {
-                            intent.ParentFollowupIntentName = intentList.Where(x => x.Id == intentResponse.ParentIntentId).FirstOrDefault().IntentName;
                         }
 
                         intent = client.CreateIntent(parent: new ProjectAgentName("ht-interview-chatbot"), intent: intent);
