dfc3d13c4ddbdeb68a5aacc5a76e95afcc604ffd
RavindraKhapate <ravi.khapate@yahoo.com>
2018-09-21 18:11:10 +0530

    Login page design modified.

--- src/HT.Interview.ChatBot.Web/ClientApp/src/app/components/login/login.component.ts
+++ src/HT.Interview.ChatBot.Web/ClientApp/src/app/components/login/login.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';             
-import { UserService } from '../../services/socialuser.service'
+import { Component, OnInit } from '@angular/core';
+import { AuthService, SocialUser, FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+import { concat } from 'rxjs/operator/concat';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor(public userService: UserService) { }
+  constructor(private socialAuthService: AuthService) { }
 
-  ngOnInit() {                     
+  ngOnInit() {
+
   }
 
+  socialSignIn(socialPlatform: string) {
+    let socialPlatformProvider;
+    if (socialPlatform == "facebook") {
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    } else if (socialPlatform == "google") {
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    }
+    console.log(socialPlatform);
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        console.log(socialPlatform + " sign in data : ", userData);
+
+      }
+    );
+  }
 }
