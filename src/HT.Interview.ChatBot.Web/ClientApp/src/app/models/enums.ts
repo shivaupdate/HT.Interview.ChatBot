f9229cc14198214f14348f74f809a3e17c5e93c7
RavindraKhapate <ravi.khapate@yahoo.com>
2018-09-28 09:43:28 +0530

    Menu enum removed.

--- src/HT.Interview.ChatBot.Web/ClientApp/src/app/models/enums.ts
+++ src/HT.Interview.ChatBot.Web/ClientApp/src/app/models/enums.ts
@@ -3,11 +3,4 @@ export enum RoleEnum {
   Admin = 2,
   HR = 3,
   Panel = 4
-} 
-
-export enum Menu {
-  Dashboard = 1,
-  InterviewEvaluation = 2,
-  ManageUsers = 3,
-  TalkToLaura = 4
-}
+}                  
