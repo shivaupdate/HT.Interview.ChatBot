d8f319531961d97549f12a069086d770bf44118f
Ravindra Khapate <Ravindra.Khapate@ey.com>
2019-08-14 17:56:28 +0530

    Code changes

--- src/HT.Interview.ChatBot.API/Program.cs
+++ src/HT.Interview.ChatBot.API/Program.cs
@@ -4,37 +4,35 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting; 
 
 namespace HT.Interview.ChatBot.API
 {
-    /// <summary>
-    /// Program
-    /// </summary>
-    public class Program
-    {
-        /// <summary>
-        /// Main
-        /// </summary>
-        /// <param name="args"></param>
-        public static void Main(string[] args)
-        { 
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        /// <summary>
-        /// Create Web Host Builder
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights();
-    }
+	/// <summary>
+	/// Program
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// Main
+		/// </summary>
+		/// <param name="args"></param>
+		public static void Main(string[] args)
+		{ 
+			CreateWebHostBuilder(args).Build().Run();
+		}
+		 
+		/// <summary>
+		/// Create Web Host Builder
+		/// </summary>
+		/// <param name="args"></param>
+		/// <returns></returns>
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.UseApplicationInsights();
+	}
 }
